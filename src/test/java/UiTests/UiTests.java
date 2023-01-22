cd2e451b703fe13f1ed06f7da918bcdd77274f74
Matvii <matveimtvcool@gmail.com>
2023-01-22 11:44:53 +0200

    Add Tests Title

--- src/test/java/UiTests/UiTests.java
+++ src/test/java/UiTests/UiTests.java
@@ -1,4 +1,22 @@
 package UiTests;
 
+import org.testng.annotations.Test;
+
 public class UiTests {
+    @Test
+    public void useCase1(){
+
+    }
+    @Test
+    public void useCase2(){
+
+    }
+    @Test
+    public void useCase3(){
+
+    }
+    @Test
+    public void useCase4(){
+
+    }
 }
