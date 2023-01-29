52dc7aa35d7762ad1f44018096eca8c1eba0afca
Matvii <matveimtvcool@gmail.com>
2023-01-29 22:05:57 +0200

    add User Controllers

--- src/test/java/ApiTests/Controllers/UserController.java
+++ src/test/java/ApiTests/Controllers/UserController.java
@@ -1,21 +1,40 @@
 package ApiTests.Controllers;
 
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import ApiTests.entities.AuthSignup;
+import ApiTests.entities.UserUpdate;
+import com.google.gson.Gson;
+import okhttp3.*;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
 
 public class UserController {
     @Test
-    public void updateUser() {
-
+    public void updateUser(UserUpdate userUpdate) throws IOException {
+        Gson gson = new Gson();
+        RequestBody body = RequestBody.create(gson.toJson(userUpdate), MediaType.parse("application/json"));
+        Request request = new Request.Builder()
+                .post(body)
+                .url("https://freelance.lsrv.in.ua/api/user/update")
+                .build();
+        OkHttpClient client = new OkHttpClient();
+        Response response = client.newCall(request).execute();
+        // AuthSignup authSignup = gson.fromJson(response.body().string(), OrdersNew.class);
+        System.out.println(response.code());
+        System.out.println(response.body());
+        System.out.println(response);
     }
 
     @Test
-    public void finedUserId() {
-
+    public void finedUserId() throws IOException {
+        Request request = new Request.Builder()
+                .get()
+                .url("https://freelance.lsrv.in.ua/api/user/29")
+                .build();
+        OkHttpClient client = new OkHttpClient();
+        Response response = client.newCall(request).execute();
+        System.out.println(response.code());
+        System.out.println(response.body().string());
     }
 
     @Test
--- src/test/java/ApiTests/entities/ImageUpload.java
+++
@@ -1,8 +0,0 @@
-package ApiTests.entities;
-
-import lombok.Data;
-
-@Data
-public class ImageUpload {
-    private  String file;
-}
