dda3cc10b939d0b93b51b5325e3f69e3a6b863a9
Matvii <matveimtvcool@gmail.com>
2023-01-30 07:52:36 +0200

    continue Api code

--- src/test/java/ApiTests/Controllers/CommentController.java
+++ src/test/java/ApiTests/Controllers/CommentController.java
@@ -1,14 +1,39 @@
 package ApiTests.Controllers;
 
+import ApiTests.entities.CommentCreate;
+import ApiTests.entities.UserUpdate;
+import com.google.gson.Gson;
+import okhttp3.*;
 import org.testng.annotations.Test;
+
+import java.io.IOException;
 
 public class CommentController {
     @Test
-    public void createComment(){
-
+    public void createComment(CommentCreate commentCreate) throws IOException {
+        Gson gson = new Gson();
+        RequestBody body = RequestBody.create(gson.toJson(commentCreate), MediaType.parse("application/json"));
+        Request request = new Request.Builder()
+                .post(body)
+                .url("https://freelance.lsrv.in.ua/api/comment/29/create")
+                .build();
+        OkHttpClient client = new OkHttpClient();
+        Response response = client.newCall(request).execute();
+        // AuthSignup authSignup = gson.fromJson(response.body().string(), OrdersNew.class);
+        System.out.println(response.code());
+        System.out.println(response.body());
+        System.out.println(response);
     }
-    @Test
-    public void getComment(){
 
+    @Test
+    public void getComment() throws IOException {
+        Request request = new Request.Builder()
+                .get()
+                .url("https://freelance.lsrv.in.ua/api/comment/29/all")
+                .build();
+        OkHttpClient client = new OkHttpClient();
+        Response response = client.newCall(request).execute();
+        System.out.println(response.code());
+        System.out.println(response.body().string());
     }
 }
--- src/test/java/ApiTests/Controllers/JobController.java
+++ src/test/java/ApiTests/Controllers/JobController.java
@@ -1,26 +1,79 @@
 package ApiTests.Controllers;
 
+import ApiTests.entities.CommentCreate;
+import ApiTests.entities.JobCreate;
+import com.google.gson.Gson;
+import okhttp3.*;
 import org.testng.annotations.Test;
+
+import java.io.IOException;
 
 public class JobController {
     @Test
-    public void deleteJobId (){
-
+    public void deleteJobId(JobCreate jobCreate) throws IOException {
+        Gson gson = new Gson();
+        RequestBody body = RequestBody.create(gson.toJson(jobCreate), MediaType.parse("application/json"));
+        Request request = new Request.Builder()
+                .post(body)
+                .url("https://freelance.lsrv.in.ua/api/job/delete/1")
+                .build();
+        OkHttpClient client = new OkHttpClient();
+        Response response = client.newCall(request).execute();
+        // AuthSignup authSignup = gson.fromJson(response.body().string(), OrdersNew.class);
+        System.out.println(response.code());
+        System.out.println(response.body());
+        System.out.println(response);
     }
-    @Test
-    public void createJob (){
 
+    @Test
+    public void createJob(JobCreate jobCreate) throws IOException {
+        Gson gson = new Gson();
+        RequestBody body = RequestBody.create(gson.toJson(jobCreate), MediaType.parse("application/json"));
+        Request request = new Request.Builder()
+                .post(body)
+                .url("https://freelance.lsrv.in.ua/api/comment/29/create")
+                .build();
+        OkHttpClient client = new OkHttpClient();
+        Response response = client.newCall(request).execute();
+        // AuthSignup authSignup = gson.fromJson(response.body().string(), OrdersNew.class);
+        System.out.println(response.code());
+        System.out.println(response.body());
+        System.out.println(response);
     }
-    @Test
-    public void getJobId (){
 
+    @Test
+    public void getJobId() throws IOException {
+        Request request = new Request.Builder()
+                .get()
+                .url("https://freelance.lsrv.in.ua/api/job/1")
+                .build();
+        OkHttpClient client = new OkHttpClient();
+        Response response = client.newCall(request).execute();
+        System.out.println(response.code());
+        System.out.println(response.body().string());
     }
-    @Test
-    public void getJobs(){
 
+    @Test //
+    public void getJobs() throws IOException {
+        Request request = new Request.Builder()
+                .get()
+                .url("https://freelance.lsrv.in.ua/api/job/all")
+                .build();
+        OkHttpClient client = new OkHttpClient();
+        Response response = client.newCall(request).execute();
+        System.out.println(response.code());
+        System.out.println(response.body().string());
     }
-    @Test
-    public void getAllJobs (){
 
+    @Test
+    public void getAllJobs() throws IOException {
+        Request request = new Request.Builder()
+                .get()
+                .url("https://freelance.lsrv.in.ua/api/job/all")
+                .build();
+        OkHttpClient client = new OkHttpClient();
+        Response response = client.newCall(request).execute();
+        System.out.println(response.code());
+        System.out.println(response.body().string());
     }
 }
